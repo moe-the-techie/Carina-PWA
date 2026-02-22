f7eea3ed4d81a1e215c41374ceb5be4967e050d9
Moe <believerahmed00@gmail.com>
2026-02-22 18:28:58 +0200

    Modified CORS allowed origins fixed

--- backend/server.js
+++ backend/server.js
@@ -68,14 +68,14 @@ const allowedOrigins = isDevelopment ? [
 ];
 
 app.use(cors({
-  // origin: function (origin, callback) {
-  //   if (!origin || allowedOrigins.includes(origin)) {
-  //     callback(null, true);
-  //   } else {
-  //     callback(new Error('Not allowed by CORS'));
-  //   }
-  // },
-  origin:true,
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  // origin:true,
   credentials: true
 }));
 
